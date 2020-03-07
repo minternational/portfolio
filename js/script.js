ab559dbcfc5b2c4bdbb323787437f066bd9a1108
minternational <mintkuehn@gmail.com>
2020-03-07 10:36:27 +0100

    my portfolio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// const impressumClick = () => {
+
+//   // const x = document.getElementById("jumbotron");
+//   // x.innerHTML = document.write = "impressum.html"
+
+//   const impressumHTML = "/impressum.html"
+//   document.getElementById("jumbotron").innerHTML = impressumHTML
+
+// };
