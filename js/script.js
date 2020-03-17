fa04de1e33b12650881f8a9d0811a1fe1ba5080a
minternational <mintkuehn@gmail.com>
2020-03-17 20:01:22 +0100

    try to add impressum event handler

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
-// function resizeIframe(obj) {
-//   obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
-// }
-
-
-// const impressumClick = () => {
-
-//   // const x = document.getElementById("jumbotron");
-//   // x.innerHTML = document.write = "impressum.html"
-
-//   const impressumHTML = "/impressum.html"
-//   document.getElementById("jumbotron").innerHTML = impressumHTML
-
-// };
+function impressum() {
+  const change = document.getElementById("context").src = "impressum.html"
+  return change;
+}
