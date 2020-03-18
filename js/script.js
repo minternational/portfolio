c1e68a90a52a2e133f1da28f74b3ca516b8c9fe0
minternational <mintkuehn@gmail.com>
2020-03-18 20:47:36 +0100

    removed js statement

--- js/script.js
+++ js/script.js
@@ -2,11 +2,3 @@ function impressum() {
   const y = document.getElementById("context").src = "impressum.html"
   document.getElementById("context").innerHTML = y;
 }
-
-
-
-
-function justATest() {
-  const time = new Date()
-  document.getElementById("context").innerHTML = time
-}
